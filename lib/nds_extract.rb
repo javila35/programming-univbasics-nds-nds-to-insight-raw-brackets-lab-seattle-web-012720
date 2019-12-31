94edf4808107f0e43ff67bd0591df3164c94d673
Joseph Avila <josepharthuravila@gmail.com>
2019-12-31 02:15:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,7 +8,7 @@ while director_index < nds.length do
   total = 0 
   while movie_index < nds[director_index][:movies].length do
     total += nds[director_index][:movies][movie_index][:worldwide_gross]
-    hash[nds[director_index][:name]] => total 
+    hash[nds[director_index][:name]] = total 
     movie_index += 1 
   end 
   movie_index = 0 
