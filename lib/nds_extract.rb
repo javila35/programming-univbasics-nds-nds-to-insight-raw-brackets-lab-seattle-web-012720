90098e7cebac8cae34a6a758d82810037a9c7464
Joseph Avila <josepharthuravila@gmail.com>
2019-12-31 02:15:18 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
+hash = {}
 movie_index= 0 
 director_index = 0 
 while director_index < nds.length do 
