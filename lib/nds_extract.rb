c7a89bd3d133691ce862d68db40aa652935197f3
Joseph Avila <josepharthuravila@gmail.com>
2019-12-31 02:03:46 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,7 +19,20 @@ end
   puts hash 
 
 def directors_totals(nds)
- 
+ movie_index= 0 
+director_index = 0 
+while director_index < nds .length do 
+  total = 0 
+  while movie_index < d_data[director_index][:movies].length do
+    total += d_data[director_index][:movies][movie_index][:worldwide_gross]
+    hash[d_data[director_index][:name]] = total 
+    movie_index += 1 
+  end 
+  movie_index = 0 
+  director_index += 1
+end 
+  
+  puts hash 
 end
 
 
